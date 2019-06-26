123595467c36206fe7b84a47afc23e97f5cfc7c3
cherifabdelmajid <44751436+cherifabdelmajid@users.noreply.github.com>
2019-06-26 15:32:26 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo, { ReactComponent } from './logo.svg';
+import './App.css';
+import MyComponent from "./Component.js"
+
+
+  function App() {
+  
+    return (
+  <div className="App">
+
+   
+  
+  <MyComponent />
+  </div> 
+   
+  );
+  }
+  
+
+    
+
+  
+
+  export default App;
---
+++ src/Component.js
@@ -0,0 +1,15 @@
+import React  from "react" ;
+
+function Item(){
+    
+    return (<div><li><img src="/image.jpg"/></li>
+    <li><img src="/image.jpg"/></li>
+    <li><img src="/image.jpg"/></li>
+    </div>
+    
+    )
+    
+}
+
+
+export default Item;
